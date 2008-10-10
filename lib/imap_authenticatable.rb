e2d072922427643efd209541713752eb64b6db04
PJ Davis <pj.davis@gmail.com>
2008-10-10 10:46:41 -0500

    changing  imap.authenticate('login', 'x','x') to imap.login('x','x'). This fixes the problem where it can't auth off of gmail accounts, because gmail doesn't support auth 'LOGIN' and return Net::IMAP::BadResponseError: TODO

--- lib/imap_authenticatable.rb
+++ lib/imap_authenticatable.rb
@@ -39,7 +39,7 @@ module CollectiveIdea
         def authenticate(username, password)
           imap = Net::IMAP.new imap_authenicatable_options[:host], imap_authenicatable_options[:port], imap_authenicatable_options[:ssl] 
           username = clean_username(username)
-          imap.authenticate('LOGIN', username, password)
+          imap.login(username, password)
           
           if imap_authenicatable_options[:allow_new_users]
             find_or_create_by_username(username)
